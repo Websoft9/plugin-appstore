5fb5bc3154adee51edb23f76c771ed986a2f5826
xuwei <76487013@qq.com>
2020-12-08 18:27:25 +0800

    use outer postgres

--- roles/awx/templates/environment.sh
+++ roles/awx/templates/environment.sh
@@ -1,7 +1,7 @@
 DATABASE_USER=awx
 DATABASE_NAME=awx
-DATABASE_HOST=localhost
+DATABASE_HOST={{{awx_docker_gateway.stdout}}
 DATABASE_PORT=5432
-DATABASE_PASSWORD=awxpass
-AWX_ADMIN_USER=admin
-AWX_ADMIN_PASSWORD=password
+DATABASE_PASSWORD={{awx_postgres_password}}
+AWX_ADMIN_USER={{awx_admin_username}}
+AWX_ADMIN_PASSWORD={{awx_admin_password}}
