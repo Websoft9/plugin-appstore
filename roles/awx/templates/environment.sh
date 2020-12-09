250afdb07c88c6f22e49410a7cd2ca7c4c0259e1
qiaofeng1227 <76487013@qq.com>
2020-12-09 14:04:55 +0800

    Update environment.sh

--- roles/awx/templates/environment.sh
+++ roles/awx/templates/environment.sh
@@ -1,6 +1,6 @@
 DATABASE_USER=awx
 DATABASE_NAME=awx
-DATABASE_HOST={{{awx_docker_gateway.stdout}}
+DATABASE_HOST={{awx_docker_gateway.stdout}}
 DATABASE_PORT=5432
 DATABASE_PASSWORD={{awx_postgres_password}}
 AWX_ADMIN_USER={{awx_admin_username}}
