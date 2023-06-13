004750ac0b46c4520d30cd87f5b52713f292eba7
zhaojing1987 <jing.zhao@websoft9.com>
2023-06-13 15:51:39 +0800

    udpate data mode

---
+++ plugins/upgrade/src/App.js
@@ -0,0 +1,15 @@
+import { Card } from 'react-bootstrap';
+
+function App() {
+  return (
+    <Card>
+      <Card.Body>
+        <div>
+          <label className="me-2 fs-5">Front End:</label>
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default App;
