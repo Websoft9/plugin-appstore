eecab66bbeff985a8192b9f2add68ecb9ea648f5
qiaofeng1227 <qiaofeng1227@users.noreply.github.com>
2023-12-05 09:23:57 +0000

    App Store conbine translation.json

--- public/po.en_US.js
+++ public/po.en_US.js
@@ -19,5 +19,225 @@ cockpit.locale({
     "W9_DB_PORT_SET": [
         null,
         "Database Port"
+    ],
+    "W9_EM_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_SYSLOG_TCP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_SYSLOG_UDP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_GELE_TCP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_GELE_UDP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_TCP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_TRANSFORM_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_SOLR_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_AMQP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_OPENWIRE_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_STOMP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_HTTPS_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_DB_INFLUXDB_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_DB_INFLUXDB_GRAPHITE_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_API_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_DB_KAFKA_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_ZOOMKEEPER_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_STATS_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_MEM_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_PANEL_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_MAGE_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_DASH_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_GUI_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_SET_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_MQ_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_ER_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_AUTH_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_WEBSOCKET_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_ES_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_GRPC_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_PORT_1_SET": [
+        null,
+        ""
+    ],
+    "W9_PORT_2_SET": [
+        null,
+        ""
+    ],
+    "W9_PORT_3_SET": [
+        null,
+        ""
+    ],
+    "W9_WEB_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_DASHBOARD_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_DB_POSTGRES_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_CLIENT_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_CLUSTER_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_ZERO_1_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_ZERO_2_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_ALPHA_1_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_ALPHA_2_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_RATEL_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_KIBANA_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_DB_ES_HTTP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_DB_ES_TCP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_LOGSTASH_BEATS_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_LOGSTASH_TCP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_LOGSTASH_UDP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_LOGSTASH_API_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_MQTT_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_RPC_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_UDP_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_KAFKA_PORT_SET": [
+        null,
+        ""
+    ],
+    "W9_ZK_PORT_SET": [
+        null,
+        ""
     ]
 });
