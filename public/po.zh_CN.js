c28d79c5ccd8864bf7521a876fddd402ba8ad28d
zhaojing1987 <jing.zhao@websoft9.com>
2024-02-27 16:27:03 +0800

    update

--- public/po.zh_CN.js
+++ public/po.zh_CN.js
@@ -8,10 +8,6 @@ cockpit.locale({
         null,
         "应用商店"
     ],
-    "My Apps": [
-        null,
-        "我的应用"
-    ],
     "All": [
         null,
         "全部"
@@ -20,9 +16,17 @@ cockpit.locale({
         null,
         "文档"
     ],
+    "Version": [
+        null,
+        "版本"
+    ],
     "App Version": [
         null,
         "应用版本"
+    ],
+    "Overview": [
+        null,
+        "概览"
     ],
     "Requires at least": [
         null,
@@ -40,42 +44,6 @@ cockpit.locale({
         null,
         "安装"
     ],
-    "Other Apps": [
-        null,
-        "其他应用"
-    ],
-    "Websoft9's Apps": [
-        null,
-        "微聚云应用"
-    ],
-    "Refresh": [
-        null,
-        "刷新"
-    ],
-    "All States": [
-        null,
-        "所有状态"
-    ],
-    "Domain": [
-        null,
-        "域名"
-    ],
-    "Access": [
-        null,
-        "访问"
-    ],
-    "Backups": [
-        null,
-        "备份"
-    ],
-    "Uninstall": [
-        null,
-        "卸载"
-    ],
-    "Updates": [
-        null,
-        "更新"
-    ],
     "Please enter a custom application name between 2 and 20 characters.Cannot start with a number.": [
         null,
         "请输入一个2-20位的自定义应用名称.不允许以数字开头."
@@ -84,217 +52,13 @@ cockpit.locale({
         null,
         "只允许使用2-20位的字母和数字,不允许使用特殊字符."
     ],
-    "Start App": [
-        null,
-        "启动应用"
-    ],
-    "Stop App": [
-        null,
-        "停止应用"
-    ],
-    "Start": [
-        null,
-        "启动"
-    ],
-    "Stop": [
-        null,
-        "停止"
-    ],
-    "Restart App": [
-        null,
-        "重启应用"
-    ],
-    "Documentation": [
-        null,
-        "文档"
-    ],
-    "This will immediately uninstall": [
-        null,
-        "这将马上卸载"
-    ],
-    "and remove all its data.": [
-        null,
-        "并且删除所有数据."
-    ],
-    "Start / Stop": [
-        null,
-        "启动 / 停止"
-    ],
-    "This will uninstall the app immediately and remove all its data.The app will be inaccessible.": [
-        null,
-        "这将立即卸载应用程序并删除其所有数据,该应用程序将无法访问."
-    ],
-    "Apps can be stopped to conserve server resources instead of uninstalling.": [
-        null,
-        "可以停止应用程序以节省服务器资源,而不是卸载."
-    ],
-    "This is the error message for": [
-        null,
-        "错误消息:"
-    ],
-    "Code: ": [
-        null,
-        "错误代码:"
-    ],
-    "Message: ": [
-        null,
-        "错误消息:"
-    ],
-    "Detail: ": [
-        null,
-        "错误详情:"
-    ],
-    "Support": [
-        null,
-        "支持"
-    ],
-    "Remove": [
-        null,
-        "删除"
-    ],
-    "Domain Binding": [
-        null,
-        "域名绑定"
-    ],
-    "Add": [
-        null,
-        "添加"
-    ],
-    "More": [
-        null,
-        "更多"
-    ],
-    "save": [
-        null,
-        "保存"
-    ],
-    "cancel": [
-        null,
-        "取消"
-    ],
-    "edit": [
-        null,
-        "编辑"
-    ],
-    "delete": [
-        null,
-        "删除"
-    ],
     "Delete": [
         null,
         "删除"
     ],
-    "default": [
-        null,
-        "默认"
-    ],
-    "set as default": [
-        null,
-        "设为默认"
-    ],
-    "Domain name cannot be empty": [
-        null,
-        "域名不能为空"
-    ],
-    "Please enter the correct domain name and cannot start with http or https!": [
-        null,
-        "请输入正确的域名,并且不能以http或者https开始！"
-    ],
-    "Are you sure you want to delete the domain for:": [
-        null,
-        "你确定删除绑定的域名："
-    ],
-    "Success": [
-        null,
-        "执行成功"
-    ],
-    "Delete domain binding": [
-        null,
-        "删除绑定域名"
-    ],
-    "saving...": [
-        null,
-        "保存中..."
-    ],
-    "Portainer": [
-        null,
-        "容器"
-    ],
-    "Container": [
-        null,
-        "容器"
-    ],
-    "Nginx": [
-        null,
-        "域名"
-    ],
-    "BackUp": [
-        null,
-        "备份"
-    ],
-    "Navigator": [
-        null,
-        "文件"
-    ],
     "Search for apps like WordPress, MySQL, GitLab, …": [
         null,
         "请输入要搜索的应用名称,例如:WordPress,MySQL,GitLab, …"
-    ],
-    "The app store is already the latest version": [
-        null,
-        "应用商店已是最新版本"
-    ],
-    "Update App Store": [
-        null,
-        "更新应用商店"
-    ],
-    "Update Log": [
-        null,
-        "更新日志"
-    ],
-    "App Store updates available": [
-        null,
-        "应用商店可升级"
-    ],
-    "Update succeeded": [
-        null,
-        "应用商店更新成功"
-    ],
-    "Update failed": [
-        null,
-        "应用商店更新失败"
-    ],
-    "App Store is upgrading……": [
-        null,
-        "应用商店正在升级中……"
-    ],
-    "The kernel version is too low,Please upgrade the kernel first.To upgrade the kernel, please go to": [
-        null,
-        "内核版本过低，请先升级内核。升级内核前往"
-    ],
-    "Due to unknown reasons, the upgrade is not possible.": [
-        null,
-        "由于未知原因，无法进行升级."
-    ],
-    "Update": [
-        null,
-        "更新"
-    ],
-    "Ignore": [
-        null,
-        "忽略"
-    ],
-    "Settings": [
-        null,
-        "设置"
-    ],
-    "Querying for updates, please wait……": [
-        null,
-        "正在查询更新，请稍后……"
-    ],
-    "Overview": [
-        null,
-        "概览"
     ],
     "Description": [
         null,
@@ -320,21 +84,13 @@ cockpit.locale({
         null,
         "添加域名"
     ],
-    "Installing...": [
+    "Domain": [
         null,
-        "安装中..."
-    ],
-    "Installation succeeded": [
-        null,
-        "安装成功"
+        "域名"
     ],
     "Port must between 1 and 65535.": [
         null,
         "端口必须在1-65535之间"
-    ],
-    "Done": [
-        null,
-        "确定"
     ],
     "Port: $0 is already in use.": [
         null,
